e8f729ab2d4b19777b2a376ab16800f601fd36ed
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2022-07-16 00:48:50 -0400

    first commit

---
+++ rnd.cpp
@@ -0,0 +1,20 @@
+//#include <boost/random/random_device.hpp>
+//#include <boost/random/uniform_int_distribution.hpp>
+#include <iostream>
+#include <random>
+using namespace std;
+int main() {
+    std::string chars(
+        "abcdefghijklmnopqrstuvwxyz"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "1234567890"
+        "!@#$%^&*()"
+        "`~-_=+[{]}\\|;:'\",<.>/? ");
+    random_device rng;
+    uniform_int_distribution<> index_dist(0, chars.size() - 1);
+    for(int i = 0; i < 8; ++i) {
+        cout << chars[index_dist(rng)];
+    }
+    cout << endl;
+}
+
